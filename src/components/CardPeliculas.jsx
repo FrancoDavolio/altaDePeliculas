ef605911607d76b9c109b3ac230787ec4bcfe072
francoDavolio <francodavolio12@gmail.com>
2022-10-19 13:13:54 -0300

    termine toda la funcionalidad, solo q el boton de enviar no estaria funcionando, no me salta ningun error y no tengo idea de q pueda ser

--- src/components/CardPeliculas.jsx
+++ src/components/CardPeliculas.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
 
-const CardPeliculas = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const CardPeliculas = ({ peliculas, borrarPelicula }) => {
+  return (
+    <div className="border my-5">
+      <div className="row justify-contet-around my-3 ms-3">
+        {peliculas.map((p, i) => {
+          return (
+            <Card style={{ width: "18rem" }} key={i}>
+              <Card.Body>
+                <Card.Title>{p.nombre}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{p.genero}</Card.Subtitle>
+                <Card.Text>{p.descripcion}</Card.Text>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    borrarPelicula(p);
+                  }}
+                >
+                  Borrar Pelicula
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default CardPeliculas;
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import {
   Form,
   Button,
@@ -7,13 +7,51 @@ import {
   Col,
   Container,
 } from "react-bootstrap";
+import CardPeliculas from "./CardPeliculas";
 
 const Formulario = () => {
+  const [id, setId] = useState(0);
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [genero, setGenero] = useState("");
+
+  const peliculasLocalStorage =
+    JSON.parse(localStorage.getItem("peliculas")) || [];
+  const [peliculas, setPeliculas] = useState(peliculasLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem("peliculas", JSON.stringify(peliculas));
+  }, [peliculas]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Desde handleSubmit");
+    const newPelicula = {
+      id: id,
+      nombre: nombre,
+      descripcion: descripcion,
+      genero: genero,
+    };
+
+    setPeliculas([...peliculas, newPelicula]);
+    setId(0);
+    setNombre("");
+    setDescripcion("");
+    setGenero("");
+  };
+
+  const borrarPelicula = (pelicula) => {
+    const arregloModificado = peliculas.filter(
+      (item) => item.id !== pelicula.id
+    );
+    setPeliculas(arregloModificado);
+  };
+
   return (
     <Container>
-      <h1>Alta de CardPeliculas</h1>
-      <br />
-      <Form>
+      <h1 className="text-center">Alta de Peliculas</h1>
+      <hr />
+      <Form onSubmit={handleSubmit}>
         <Row className="justify-content-center">
           <Form.Group
             className="mb-3 my-4"
@@ -22,7 +60,12 @@ const Formulario = () => {
             md="7"
           >
             <Form.Label>Nombre de la Pelicula:</Form.Label>
-            <Form.Control type="text" placeholder="Nombre de la Pelicula" />
+            <Form.Control
+              type="text"
+              placeholder="Nombre de la Pelicula"
+              onChange={(e) => setNombre(e.target.value)}
+              value={nombre}
+            />
           </Form.Group>
           <FloatingLabel
             controlId="floatingTextarea2"
@@ -30,6 +73,8 @@ const Formulario = () => {
             as={Col}
             md="7"
             className="my-4"
+            onChange={(e) => setDescripcion(e.target.value)}
+            value={descripcion}
           >
             <Form.Control as="textarea" style={{ height: "100px" }} />
           </FloatingLabel>
@@ -38,17 +83,24 @@ const Formulario = () => {
             md="12"
             className="my-4"
             style={{ width: "740px" }}
+            onChange={(e) => setGenero(e.target.value)}
+            value={genero}
           >
             <option>Genero:</option>
-            <option value="1">comedia</option>
-            <option value="2">drama</option>
-            <option value="3">infantil</option>
+            <option value="1">Comedia</option>
+            <option value="2">Drama</option>
+            <option value="3">Infantil</option>
           </Form.Select>
+          <hr />
           <Button variant="primary" type="submit" as={Col} md="6">
-            Submit
+            Enviar
           </Button>
         </Row>
       </Form>
+      <CardPeliculas
+        peliculas={peliculas}
+        borrarPelicula={borrarPelicula}
+      ></CardPeliculas>
     </Container>
   );
 };
