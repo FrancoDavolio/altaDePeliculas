faf0e8f5ac21b0a53b2a97af3f85a3e5e33dabbf
francoDavolio <francodavolio12@gmail.com>
2022-10-19 11:39:39 -0300

    first commit

---
+++ src/components/CardPeliculas.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CardPeliculas = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CardPeliculas;
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Formulario = () => {
+    return (
+        <div>
+            <h1>Alta de CardPeliculas</h1> 
+            <br />
+        </div>
+    );
+};
+
+export default Formulario;
